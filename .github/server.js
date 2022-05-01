90d2a6f00110c6b5d2e4b677577ab83267ccdc61
Flifli-jm <lili1@live.unc.edu>
2022-04-30 22:50:22 -0400

    change

--- .github/server.js
+++ .github/server.js
@@ -8,36 +8,6 @@ const args = minimist(process.argv.slice(2));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
-const HTTP_PORT = args["port"] || 5555;
-
-// Store help text 
-const help = (`
-server.js [options]
-
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
-
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
-
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
-
---help	Return this message and exit.
-`)
-
-// If --help or -h, echo help text to STDOUT and exit
-if (args.help || args.h) {
-    console.log(help)
-    process.exit(0)
-}
-//start an app server
-const server = app.listen (HTTP_PORT, () => {
-    console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
-});
 //loging db
 app.use((req, res, next) => {
     let logdata = {
@@ -68,12 +38,42 @@ app.use((req, res, next) => {
     next();
 })
 args["port", "debug", "log", "help"];
+const HTTP_PORT = args["port"] || 5555;
+
+// Store help text 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+//start an app server
+const server = app.listen (HTTP_PORT, () => {
+    console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
+});
+
 if (args.debug) {
-    app.get('/app/log/access/', (req, res) => {
-            const stmt = db.prepare("SELECT * FROM accesslog").all();
+    app.get('/app/log/access/', (req, res,next) => {
+            const stmt = db.prepare('SELECT * FROM accesslog').all()
 	        res.status(200).json(stmt); 
     })
-    app.get('/app/error/', (req, res) => {
+    app.get('/app/error/', (req, res,next) => {
         throw new Error('Error test success')
     })
 }
@@ -83,7 +83,7 @@ app.get("/app/", (req, res,next) => {
 });
 
 
-app.get("/app/flip/", (req, res) => {
+app.get("/app/flip/", (req, res,next) => {
     var flip = coinFlip()
     return res.status(200).json({
         "flip" : coinFlip()
@@ -91,7 +91,7 @@ app.get("/app/flip/", (req, res) => {
 })
 
 
-app.get('/app/flips/:number', (req, res) => {
+app.get('/app/flips/:number', (req, res,next) => {
     const raw = coinFlips(req.params.number);
     const summary = countFlips(raw);
     res.status(200).json({
@@ -102,11 +102,11 @@ app.get('/app/flips/:number', (req, res) => {
 
 
 
-app.get("/app/flip/call/heads", (req, res) => {
+app.get("/app/flip/call/heads", (req, res,next) => {
     return res.status(200).json(flipACoin("heads"))
 })
 
-app.get("/app/flip/call/tails", (req, res) => {
+app.get("/app/flip/call/tails", (req, res,next) => {
     return res.status(200).json(flipACoin("tails"))
 })
 
